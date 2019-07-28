cc29c434baee6bc6e2de4c91ba7cc4dda6104ea5
nethjai <marlapatinethaji@gmail.com>
2019-07-28 17:05:56 +0000

    iam_authen-kubectl-eksctl-install

--- iam_authen-kubectl-eksctl-install.sh
+++ iam_authen-kubectl-eksctl-install.sh
@@ -2,15 +2,10 @@ echo "Installing aws-iam-authenticator "
 
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
 
-curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator.sha256
-
-openssl sha1 -sha256 aws-iam-authenticator
 
 chmod +x ./aws-iam-authenticator
 
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
-
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+sudo mv aws-iam-authenticator /usr/local/bin/
 
 aws-iam-authenticator version
 
